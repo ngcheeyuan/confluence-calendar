d8eff410ac274a24b8a9f8e884eb81461f27210e
ngcheeyuan <gideonng89@gmail.com>
2026-01-10 06:46:57 +0800

    updating constants

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 # API Paths
 CAL_EVENT_PATH = "rest/calendar-services/1.0/calendar/events.json"
 SUB_CAL_LIST_PATH = "rest/calendar-services/1.0/calendar/subcalendars.json"
+SPACE_SEARCH_PATH = "rest/prototype/1/search/name.json"
+ICS_IMPORT_PATH = "plugins/calendar/importsubcalendar.action"
 
-# Local Settings
+# Settings
 USER_TIMEZONE = "Asia/Taipei"
-
-# HTTP Headers
 HEADERS = {
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     "X-Requested-With": "XMLHttpRequest"
